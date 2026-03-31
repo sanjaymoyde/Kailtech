0cfbec5eaebf2dce765648a0f5aec1080ad02bf8
Sanjay Moyde <sanjaymoydey00786@gmail.com>
2026-03-31 18:09:00 +0530

    feat: implement user profile and header components for Sideblock and MainLayout layouts

--- src/app/layouts/MainLayout/Header/index.jsx
+++ src/app/layouts/MainLayout/Header/index.jsx
@@ -8,7 +8,6 @@ import { SidebarToggleBtn } from "components/shared/SidebarToggleBtn";
 import { Button } from "components/ui";
 import { Notifications } from "components/template/Notifications";
 import { RightSidebar } from "components/template/RightSidebar";
-import { LanguageSelector } from "components/template/LaguageSelector";
 import { Search } from "components/template/Search";
 import { useThemeContext } from "app/contexts/theme/context";
 
@@ -77,7 +76,6 @@ export function Header() {
         />
         <Notifications />
         <RightSidebar />
-        <LanguageSelector />
       </div>
     </header>
   );
